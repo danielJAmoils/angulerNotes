8d87b8314bf40fbfd6f9b585d77ca8e1aa5e0a37
Daniel Amoils <danieljamoils@gmail.com>
2020-07-17 23:55:55 -0400

    class decerators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,24 @@
 import { Component } from '@angular/core';
 
-export function log(target:object, name:string, descriptor:PropertyDescriptor){
-  //console.log(target, name, descriptor)
-  //store the original function in a variable
-  const original = descriptor.value
-  //do some manipulations with descriptor.value function
-
-  descriptor.value = function(...args){
-    console.log("Arguments ", args, "were passed in this function")
-    const result:number = original.apply(this, args)
-    console.log("the result of the function is", result)
-    return result
+export function log<T extends { new(...args: any[]): {} }>(className: T) {//function taken from stack overflow as one from tutorial was not working
+  console.log(className)
+  return class extends className {
+    constructor(...args) {
+      super(...args);
+      console.log("Arguments passed to this class's constructor are ", args)//stackoverflow link: https://stackoverflow.com/questions/58188850/unable-to-resolve-signature-of-class-decorator-when-called-as-an-expression
+    }
   }
+} 
 
-  //return descriptor
-  return descriptor
+
+@log
+class myExampleClass{
+  constructor(arg1, arg2) {
+    console.log("Contructor fired")
+  }
 }
+
+const myClass = new myExampleClass(5, 10)
 
 @Component({
   selector: 'app-root',
@@ -29,7 +32,6 @@ export class AppComponent {
     console.log("this statement was made by the constructor",this.aSimpleMethod(5))
   }
 
-  @log//member decorator
   aSimpleMethod(a: number){
     return a*a
   }
