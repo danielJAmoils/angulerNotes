e99ddca6dae01040505e3fa75b0185746330be9e
Daniel Amoils <danieljamoils@gmail.com>
2020-07-18 23:00:43 -0400

    interpolation and expressions

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HelloComponent implements OnInit {
 
+  myVariable = "Daniel"
+  bitwiseOR = 2 | 5 //010 | 101 => 0 | 1 is 1 then 1 or 0 is 1 then 0 | 1 is 1 = 111 or 7
+  i = 0
+  someVariable = Math.random()
+
   constructor() { }
 
+  doSomeHeavyTask(){
+    console.log(`called ${this.i++} times`)
+  }
+
   ngOnInit(): void {
+    setInterval(() => {
+      this.someVariable = Math.random()//every time number is updated heavy task is called
+    }, 50)
   }
 
 }
