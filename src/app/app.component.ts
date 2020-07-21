945280871ff3c993422e154d6e023815f40bbd48
Daniel Amoils <danieljamoils@gmail.com>
2020-07-21 19:33:44 -0400

    source to target data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'intro2angular';
+  myVariable = 'intro2angular';
+  myDisabledValue = false
+  constructor(){
+    this.myVariable = ""
+    this.myDisabledValue = false
+    setInterval(() => {
+      this.myVariable = Math.random().toString()
+      this.myDisabledValue = Math.random() > 0.5
+    }, 500)
+  }
+
+  callMyFunction(){
+    
+  }
 }
